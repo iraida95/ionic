3065d0f97f3694d7f71ce6df979d07a118197368
Iraida <ira-ida_95@hotmail.com>
2017-04-06 15:55:53 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  rootPage = TabsPage;
+
+    pages: Array<{title: string, component: any}>;
+
+    constructor(platform: Platform) {
+
+      platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
+
